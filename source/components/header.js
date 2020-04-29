1d6044ed3a33c3c51f2db760929454c4c4f01610
Kengarut <kengarut@gmail.com>
2020-04-30 00:43:11 +0300

    Create backdrop mobile menu

--- source/components/header.js
+++ source/components/header.js
@@ -13,75 +13,88 @@ export default class Header extends React.Component{
 		this.menuHeader = React.createRef();
 		this.itemMenu =  React.createRef();
 
-		this.hideHeader = false;
 		this.point = false;
 	}
 
-	swipe(){
+	mobileBackdrop(){
+		if(window.innerWidth < 900){
+			this.mainContainer = document.getElementById("main");
+			this.backMobile = document.createElement("div");
+			this.backMobile.className = "backdrop-mobile";
+			this.mainContainer.appendChild(this.backMobile);
+		}
+	}
 
+	removeMobileBackdrop(){
+		if( window.innerWidth > 900 ){
+			this.mainContainer = document.getElementById("main");
+			this.backMob =  document.getElementsByClassName("backdrop-mobile")[0];
+			this.mainContainer.removeChild(	this.backMob);
+			console.log(	this.backMob, 555);
+		}
+	}
+
+	toggleMobileBackdrop(){
+		if( window.innerWidth > 900){
+			this.mobileBackdrop();
+		}
+	}
+
+	swipe(){
 		if( this.point ){
 			console.log(111);
 			this.menuHeader.current.classList.remove("show-menu");
 			this.menuHeader.current.classList.add("hide-menu");
-			this.itemMenu.current.style.display="none";
+
+			this.itemMenu.current.classList.remove("bar-block-menu-header");
+			this.itemMenu.current.classList.add("hide-menu-header-items");
 			this.point = false;
 		}else{
 			console.log(222);
 			this.menuHeader.current.classList.remove("hide-menu");
 			this.menuHeader.current.classList.add("show-menu");
-			this.itemMenu.current.style.display="flex";
-			this.itemMenu.current.classList.add("fd-column");
+
+			this.itemMenu.current.classList.remove("hide-menu-header-items");
+			this.itemMenu.current.classList.add("bar-block-menu-header");
 
 			this.point = true;
 		}
-
 	}
 
-	hidePointHeader(){
-		if(window.innerWidth < 900 ){
-			console.log(777);
-			this.itemMenu.current.style.display="none";
-		}else{
+	zeroingClassesHeaderMobile (){
+		if(window.innerWidth > 900 ){
 			console.log(2020);
+			this.menuHeader.current.classList.remove("hide-menu");
+			this.menuHeader.current.classList.remove("show-menu");
+
+			this.itemMenu.current.classList.remove("hide-menu-header-items");
+			this.itemMenu.current.classList.remove("bar-block-menu-header");
 			this.itemMenu.current.style.display="flex";
 		}
-
 	}
 
 	onresize(){
-
 		if( window.innerWidth < 900 ){
-			this.btnMenu.current.style.display="block";
 			console.log( 333 );
+			this.btnMenu.current.style.display="block";
+			this.itemMenu.current.classList.add("hide-menu-header-items");
 		}else{
 			console.log( 444 );
 			this.btnMenu.current.style.display="none";
 		}
-
-		if( window.innerWidth > 900  ){
-			this.menuHeader.current.classList.remove("hide-menu");
-			this.menuHeader.current.classList.remove("show-menu");
-		}
-
-		if( window.innerWidth < 900){
-			this.hidePointHeader();
-			this.itemMenu.current.classList.remove("fd-column");
-		}
-
-		if( window.innerWidth > 900){
-			this.hidePointHeader();
-		}
+		this.zeroingClassesHeaderMobile();
 	};
-
-
 
 	componentDidMount(){
 
 		window.addEventListener( "resize", () => this.onresize());
+		window.addEventListener("onload",()=>this.toggleMobileBackdrop());
 		this.onresize();
-		this.hidePointHeader();
-		this.btnMenu.current.addEventListener( "click", () => {
+		this.toggleMobileBackdrop();
+		//this.mobileBackdrop();
+		//this.removeMobileBackdrop();
 
+		this.btnMenu.current.addEventListener( "click", () => {
 			if( window.innerWidth < 900 ){
 				this.swipe();
 			}
